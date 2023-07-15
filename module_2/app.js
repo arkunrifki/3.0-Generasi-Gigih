0971601e8e06ff34fb98502c5beffb4f9cb6058a
Arkun Rifki <arkunrifki17@gmail.com>
2023-07-16 05:52:48 +0700

    Adding homework module 2.2

--- module_2/app.js
+++ module_2/app.js
@@ -1,14 +1,78 @@
-const http = require("http");
-
-const hostname = "127.0.0.1";
+const express = require("express"); //Framework NodeJS dengan mengambile module express
+const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
+//Routes / URL / endpoint utama dengan method GET
+
+//masukkin body-parser agar bisa dimasukkan ke POST
+app.use(bodyParser.json());
+//Mengambil format dari frontEnd yang ngirim ke saya sebuah request berupa post method lalu ubah ke JSON format
+//dari depan udah dikirim JSON format nah saya ambil dengan format JSON format juga
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World! Bismillah dapet kerja");
+// });
+
+// app.get("/hy", (req, res) => {
+//   console.log({ urlParam: req.query.alamat });
+//   res.send("Bulan ini dapet kerja!!!!");
+// });
+
+// app.post("/login", (req, res) => {
+//   console.log({ requestFromOutside: req.body });
+//   res.send("login berhasil");
+// });
+
+// app.put("/username", (req, res) => {
+//   console.log({ updateData: req.body });
+//   res.send("update berhasil");
+// });
+
+app.use(express.json());
+
+let songs = [
+  {
+    id: 1,
+    title: "Just The Way You Are",
+    artist: "Bruno Mars",
+  },
+  {
+    id: 2,
+    title: "Talking to the Moon",
+    artist: "Bruno Mars",
+  },
+  {
+    id: 3,
+    title: "Treasure",
+    artist: "Bruno Mars",
+  },
+  {
+    id: 4,
+    title: "24K Magic",
+    artist: "Bruno Mars",
+  },
+];
+
+//Read songs from data
+app.get("/songs", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.send(JSON.stringify(songs));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+//Read song by id
+app.get("/songs/id", (req, res) => {
+  const id = req.params.id; /// mengambil params id dan memasukkan nya ke const id
+  const song = songs.find((song) => song.id === id); // menggunakan find untuk mencari object lagu dalam array songs
+  // buat percabangan untuk menerima json id dari const music, jika tidak ada maka akan ada status error
+  if (song) {
+    res.json(song);
+  } else {
+    res.status(404).json({ message: "Not found song you search" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
